dafb9284dc781d341e5cfc5756c5cbcb2373f8b5
박성언 <tjddjs909@naver.com>
2024-05-16 10:25:07 +0900

    test

--- src/index.js
+++ src/index.js
@@ -6,16 +6,22 @@ import reportWebVitals from './reportWebVitals';
 import Library1 from './chapter03/Library1';
 import StudentList from './chapter03/StudentList';
 import ConfirmDialog from './chapter04/ConfirmDialog';
+import Clock from './chapter04/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    {/* <Library1/> */}
-    {/* <StudentList/> */}
-    <ConfirmDialog />
-  </React.StrictMode>
-);
+setInterval( () => {
+  root.render(
+    <React.StrictMode>
+      {/* <App /> */}
+      {/* <Library1/> */}
+      {/* <StudentList/> */}
+      {/* <ConfirmDialog /> */}
+      <Clock />
+    </React.StrictMode>
+  );
+}, 1000)
+//setInterval 함수를 사용해서, 화면이 1초마다 다시 부르도록 함.
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
