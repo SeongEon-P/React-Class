2b1134892a6c84e89056b7ed1a2649d58e6c971d
박성언 <tjddjs909@naver.com>
2024-05-16 16:43:34 +0900

    react class_ch6

--- src/index.js
+++ src/index.js
@@ -7,20 +7,28 @@ import Library1 from './chapter03/Library1';
 import StudentList from './chapter03/StudentList';
 import ConfirmDialog from './chapter04/ConfirmDialog';
 import Clock from './chapter04/Clock';
+import PropsEx from './chapter05/PropsEx';
+import CommentList from './chapter05/CommentList';
+import BoardList from './chapter05/BoardList';
+import NotificationList from './chapter06/NotificationList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-setInterval( () => {
+// setInterval( () => {
   root.render(
     <React.StrictMode>
       {/* <App /> */}
       {/* <Library1/> */}
       {/* <StudentList/> */}
       {/* <ConfirmDialog /> */}
-      <Clock />
+      {/* <Clock /> */}
+      {/* <PropsEx /> */}
+      {/* <CommentList /> */}
+      {/* <BoardList /> */}
+      <NotificationList />
     </React.StrictMode>
   );
-}, 1000)
-//setInterval 함수를 사용해서, 화면이 1초마다 다시 부르도록 함.
+// }, 1000)
+//setInterval 함수를 사용해서, 끝에 "}, 1000)"이렇게 하면서 화면이 1초마다 다시 부르도록 함.
 
 
 // If you want to start measuring performance in your app, pass a function
